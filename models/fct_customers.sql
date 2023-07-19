ce1e57021a7cd4919ffd61bb210c7c023f68990a
dbtjava@proton.me <dbtjava@proton.me>
2023-07-19 17:01:46 +0000

    fct_customers__add-fullname_AND_orderBy-numbers-of-orders

--- models/fct_customers.sql
+++ models/fct_customers.sql
@@ -6,10 +6,12 @@ with
 final as(
     select 
     customer_id,
+    first_name||' '||last_name as full_name,
     number_of_orders
     from {{ ref('dim_customers') }}
     -- remove customers with no orders
     where number_of_orders > 0
+    order by number_of_orders desc
 )
 
 select * from final
